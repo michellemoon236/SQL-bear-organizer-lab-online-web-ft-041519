4d4da5ca719203402fca3117e71479b82c08180e
Michelle Moon <michellemoon236@gmail.com>
2019-05-15 20:51:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,26 +1,26 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 5, "male", "brown", "happy"
+    VALUES ("Mr. Chocolate", 5, "male", "brown", "happy", 
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 10, "female", "yellow", "moody"  
+    VALUES ("Rowdy", 10, "female", "yellow", "moody",  
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 1,"female", "black", "sad"
+    VALUES ("Tabitha", 1,"female", "black", "sad", 
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", 1, "male", "brown", "angry"
+    VALUES ("Sergeant Brown", 1, "male", "brown", "angry", 
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 3,"female", "yellow", "happy"      
+    VALUES ("Melissa", 3,"female", "yellow", "happy",      
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 2, "male","brown",    
+    VALUES ("Grinch", 2, "male","brown", "moody",    
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 8,"female", "yellow"  
+    VALUES ("Wendy", 8,"female", "yellow", "sad",   
     
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES (NULL, 2, "male", "black",   
+    VALUES (NULL, 2, "male", "black", "angry",   
     
   id INTEGER PRIMARY KEY,
   name TEXT,
